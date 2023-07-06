12f94c3f40194a378458b0f9c0d873d1c59f0174
David Stevenson <drstevenson1958@yahoo.com>
2023-07-06 17:27:47 -0400

    Sorting a List Temporarily with the sorted() Function

--- ch03/cars.py
+++ ch03/cars.py
@@ -9,3 +9,23 @@ cars = [ 'bmw', 'audi', 'toyota', 'subaru' ]
 cars.sort(reverse=True)
 print(cars)
 # ['toyota', 'subaru', 'bmw', 'audi']
+
+# Sorting a List Temporarily with the sorted() Function
+cars = [ 'bmw', 'audi', 'toyota', 'subaru' ]
+print("\nHere is the original list")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+# Here is the original list
+# ['bmw', 'audi', 'toyota', 'subaru']
+
+# Here is the sorted list:
+# ['audi', 'bmw', 'subaru', 'toyota']
+
+# Here is the original list again:
+# ['bmw', 'audi', 'toyota', 'subaru']
