0854f98bf3e7de45b5cd7abe4813f0c958f1a229
David Stevenson <drstevenson1958@yahoo.com>
2023-07-06 17:17:59 -0400

    # Popping Items from any Position in a List

--- ch03/motorcycles.py
+++ ch03/motorcycles.py
@@ -47,3 +47,9 @@ motorcycles = [ 'honda', 'yamaha', 'suzuki' ]
 last_owned = motorcycles.pop()
 print(f"The last motorcycle I owned was a {last_owned.title()}")
 # The last motorcycle I owned was a Suzuki
+
+# Popping Items from any Position in a List
+motorcycles = [ 'honda', 'yamaha', 'suzuki' ]
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}")
+# The first motorcycle I owned was a Honda
