74047d8ecd53218866c9675d8090d0ef82c46fe6
David Stevenson <drstevenson1958@yahoo.com>
2023-07-05 22:04:16 -0400

    Stripping Whitespace

--- ch02/whitespace.py
+++ ch02/whitespace.py
@@ -3,3 +3,7 @@ print("Languages:\nPython\nC\nJavaScript")
 
 favorite_language = 'python '
 print(f"\"{favorite_language.rstrip()}\"")
+
+favorite_language = '   python   '
+print(f"\"{favorite_language.lstrip()}\"")
+print(f"\"{favorite_language.strip()}\"")
