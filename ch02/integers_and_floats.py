de866adee354cc81cada1d666862359886ea14e7
David Stevenson <drstevenson1958@yahoo.com>
2023-07-05 22:19:50 -0400

    Integers and Floats Together

---
+++ ch02/integers_and_floats.py
@@ -0,0 +1,6 @@
+print(f"4 / 2 = {4 / 2}")
+print(f"1 + 2.0 = {1 + 2.0}")
+print(f"2 * 3.0 = {2 * 3.0}")
+print(f"3.0 ** 2 = {3.0 ** 2}")
+
+# Python defaults to a float in any operatoin that uses a float, even if the output is a whole number.
