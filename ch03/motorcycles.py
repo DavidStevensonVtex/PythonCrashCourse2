59239852edd86b1c7a3c4a8834ab170c4049147c
David Stevenson <drstevenson1958@yahoo.com>
2023-07-06 16:14:20 -0400

    Removing an Item using the del Statement

--- ch03/motorcycles.py
+++ ch03/motorcycles.py
@@ -24,3 +24,11 @@ motorcycles = [ 'honda', 'yamaha', 'suzuki' ]
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
 ['ducati', 'honda', 'yamaha', 'suzuki']
+
+# Removing an Item using the del Statement
+motorcycles = [ 'honda', 'yamaha', 'suzuki' ]
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+# ['honda', 'yamaha', 'suzuki']
+# ['yamaha', 'suzuki']
