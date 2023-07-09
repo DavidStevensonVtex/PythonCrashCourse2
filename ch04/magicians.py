8282572d4b7d0909cf7df2a625f562977c464ee0
David Stevenson <drstevenson1958@yahoo.com>
2023-07-09 19:29:21 -0400

    Indenting Unnecessarily After the Loop

--- ch04/magicians.py
+++ ch04/magicians.py
@@ -1,12 +1,18 @@
 magicians = [ 'alice', 'david', 'carolina' ]
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
-print(f"I can't wait to see your next trick, {magician.title()}.\n")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
-print("Thank you, everyone. That was a great magic show!")
+    print("Thank you, everyone. That was a great magic show!")
 
 # Alice, that was a great trick!
+# I can't wait to see your next trick, Alice.
+
+# Thank you, everyone. That was a great magic show!
 # David, that was a great trick!
+# I can't wait to see your next trick, David.
+
+# Thank you, everyone. That was a great magic show!
 # Carolina, that was a great trick!
 # I can't wait to see your next trick, Carolina.
 
