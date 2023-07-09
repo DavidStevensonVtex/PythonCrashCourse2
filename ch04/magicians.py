d56af29416cd5c3345cd23628eaa76d283bdde57
David Stevenson <drstevenson1958@yahoo.com>
2023-07-09 19:26:32 -0400

    Forgetting to Indent Additional Lines

--- ch04/magicians.py
+++ ch04/magicians.py
@@ -1,17 +1,13 @@
 magicians = [ 'alice', 'david', 'carolina' ]
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
-    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
-print("Thank you, everyone. That was agreat magic show!")
+print("Thank you, everyone. That was a great magic show!")
 
 # Alice, that was a great trick!
-# I can't wait to see your next trick, Alice.
-
 # David, that was a great trick!
-# I can't wait to see your next trick, David.
-
 # Carolina, that was a great trick!
 # I can't wait to see your next trick, Carolina.
 
-# Thank you, everyone. That was agreat magic show!
+# Thank you, everyone. That was a great magic show!
