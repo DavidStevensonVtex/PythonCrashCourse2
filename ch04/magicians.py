2a068520852091d7503218a5c828ff3354c36a3f
David Stevenson <drstevenson1958@yahoo.com>
2023-07-09 19:19:39 -0400

    Doing MOre Work Within a for Loop

--- ch04/magicians.py
+++ ch04/magicians.py
@@ -1,3 +1,13 @@
 magicians = [ 'alice', 'david', 'carolina' ]
 for magician in magicians:
-    print(magician)
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# Alice, that was a great trick!
+# I can't wait to see your next trick, Alice.
+
+# David, that was a great trick!
+# I can't wait to see your next trick, David.
+
+# Carolina, that was a great trick!
+# I can't wait to see your next trick, Carolina.
