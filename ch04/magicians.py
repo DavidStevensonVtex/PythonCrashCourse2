2197ff8ddb1e57c3f3fde24f2f83f0fa2e11ac50
David Stevenson <drstevenson1958@yahoo.com>
2023-07-09 19:32:25 -0400

    Forgetting The Colon

--- ch04/magicians.py
+++ ch04/magicians.py
@@ -2,4 +2,4 @@ magicians = [ 'alice', 'david', 'carolina' ]
 for magician in magicians
     print(magician)
                             #  ^
-SyntaxError: invalid syntax
+# SyntaxError: invalid syntax
