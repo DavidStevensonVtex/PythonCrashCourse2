3077fbab53766f336e7406dadc3dd3e92b86603a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-09 19:58:34 -0400

    List Comprehensions

--- ch04/squares.py
+++ ch04/squares.py
@@ -1,7 +1,3 @@
-squares = []
-for value in range(1,11):
-    square = value ** 2
-    squares.append(square)
-
+# Using List Comprehensions
+squares = [ value ** 2 for value in range(1, 11)]
 print(squares)
-# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
