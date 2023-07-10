8d2ff0114bc7af468e0ea74b263ee3cec5e6abb0
David Stevenson <drstevenson1958@yahoo.com>
2023-07-09 20:19:51 -0400

    Copying a List using the Slice Operator

---
+++ ch04/copying_a_list.py
@@ -0,0 +1,14 @@
+my_foods = [ 'pizza', 'falafel', 'carrot cake' ]
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+# My favorite foods are:
+# ['pizza', 'falafel', 'carrot cake']
+
+# My friend's favorite foods are:
+# ['pizza', 'falafel', 'carrot cake']
