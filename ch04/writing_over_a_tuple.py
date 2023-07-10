5685a7a4a9d26c9340c9417bf6138f3e6833cbfb
David Stevenson <drstevenson1958@yahoo.com>
2023-07-10 07:11:12 -0400

    Writing over a tuple

---
+++ ch04/writing_over_a_tuple.py
@@ -0,0 +1,17 @@
+dimensions = (200,50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+print("\nModified dimensions:")
+dimensions = (400,100)
+for dimension in dimensions:
+    print(dimension)
+
+# Original dimensions:
+# 200
+# 50
+
+# Modified dimensions:
+# 400
+# 100
