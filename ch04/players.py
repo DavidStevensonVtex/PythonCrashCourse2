78288920c6b4fdcecb38d188384b54b5002b32d7
David Stevenson <drstevenson1958@yahoo.com>
2023-07-09 20:12:17 -0400

    Slicing a List

--- ch04/players.py
+++ ch04/players.py
@@ -1,3 +1,16 @@
 players = [ 'charles', 'martina', 'michael', 'florence', 'eli' ]
 print(players[0:3])
 # ['charles', 'martina', 'michael']
+
+print(players[1:4])
+# ['martina', 'michael', 'florence']
+
+print(players[:4])
+# ['charles', 'martina', 'michael', 'florence']
+
+print(players[2:])
+# ['michael', 'florence', 'eli']
+
+# This prints the last three players.
+print(players[-3:])
+# ['michael', 'florence', 'eli']
