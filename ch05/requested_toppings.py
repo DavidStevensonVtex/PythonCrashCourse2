9f58ae70fcfd86bf910578ad496dd1a3d5531819
David Stevenson <drstevenson1958@yahoo.com>
2023-07-11 19:54:47 -0400

    Checking for Special Items in a loop

--- ch05/requested_toppings.py
+++ ch05/requested_toppings.py
@@ -1,12 +1,9 @@
-requested_toppings = [ 'mushrooms', 'onions', 'pineapple', 'extra cheese' ]
+requested_toppings = [ 'mushrooms', 'green peppers', 'extra cheese' ]
 
-if 'mushrooms' in requested_toppings:
-    print('adding mushrooms')
-
-if 'pepperoni' in requested_toppings:
-    print('adding pepperoni')
-
-if 'extra cheese' in requested_toppings:
-    print('adding extra cheese')
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}")
 
 print("\nFinished making your pizza!")
