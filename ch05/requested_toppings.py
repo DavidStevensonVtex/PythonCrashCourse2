08af6f9c5e4892b3c692ef678dbd896070a29438
David Stevenson <drstevenson1958@yahoo.com>
2023-07-11 19:30:08 -0400

    Checking whether a value is in a list

---
+++ ch05/requested_toppings.py
@@ -0,0 +1,11 @@
+requested_toppings = [ 'mushrooms', 'onions', 'pineapple' ]
+
+if 'mushrooms' in requested_toppings:
+    print('mushrooms have been requested')
+else:
+    print('mushrooms have not been requested')
+
+if 'pepperoni' in requested_toppings:
+    print('pepperoni has been requested')
+else:
+    print('pepperoni has not been requested')
