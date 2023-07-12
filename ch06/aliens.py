1a8a8071d2784e4f2840ffc5328a202092ac8e95
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 18:53:54 -0400

    Modify yellow aliens to make them red, fast and 15 points

--- ch06/aliens.py
+++ ch06/aliens.py
@@ -11,6 +11,10 @@ for alien in aliens[:3]:
         alien['color'] = 'yellow'
         alien['speed'] = 'medium'
         alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15       
 
 # Show the first 5 aliens.
 print("First 5 aliens.")
