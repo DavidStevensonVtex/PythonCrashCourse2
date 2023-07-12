76629172fdeebd87ae31590c79914d90136a12d8
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 17:51:29 -0400

    Removing Key-Value Pairs

--- ch06/alien.py
+++ ch06/alien.py
@@ -1,20 +1,7 @@
-alien_0 = { 'x_position': 0, 'y_position': 25, 'speed': 'medium' }
-print(f"Original x position: {alien_0['x_position']}  Speed: {alien_0['speed']}")
+alien_0 = { 'color': 'green', 'points': 5 }
+print(alien_0)
 
-# Move the alien to the right.
-# Determine how far to move the alien based on its current speed.
-
-if alien_0['speed'] == 'slow':
-    x_increment = 1
-elif alien_0['speed'] == 'medium':
-    x_increment = 2
-else:
-    # This must be a fast alien.
-    x_increment = 3
-
-# The new position is the old position plus the increment.
-alien_0['x_position'] = alien_0['x_position'] + x_increment
-print(f"New x position: {alien_0['x_position']}")
-
-# Original x position: 0  Speed: medium
-# New x position: 2
+del alien_0['points']
+print(alien_0)
+# {'color': 'green', 'points': 5}
+# {'color': 'green'}
