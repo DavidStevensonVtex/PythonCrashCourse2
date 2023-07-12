9f2c16d9ea033ac889013710998547d7acfd8316
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 18:13:35 -0400

    Looping through all the key-value pairs

--- ch06/favorite_languages.py
+++ ch06/favorite_languages.py
@@ -8,7 +8,15 @@ favorite_languages = {
 print(favorite_languages)
 
 language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}")
+print(f"Sarah's favorite language is {language}\n")
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}")
 
 # {'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python'}
 # Sarah's favorite language is C
+
+# Jen's favorite language is Python
+# Sarah's favorite language is C
+# Edward's favorite language is Ruby
+# Phil's favorite language is Python
