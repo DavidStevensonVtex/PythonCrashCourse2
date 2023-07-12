e40eff658d000137000c3408416c322188a26621
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 17:47:56 -0400

    Modifying Values in a Dictionary - 2

--- ch06/alien.py
+++ ch06/alien.py
@@ -1,7 +1,20 @@
-alien_0 = {'color': 'green'}
-print(f"The alien is {alien_0['color']}.")
+alien_0 = { 'x_position': 0, 'y_position': 25, 'speed': 'medium' }
+print(f"Original x position: {alien_0['x_position']}  Speed: {alien_0['speed']}")
 
-alien_0['color'] = 'yellow'
-print(f"The alien is now {alien_0['color']}.")
-# The alien is green.
-# The alien is now yellow.
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New x position: {alien_0['x_position']}")
+
+# Original x position: 0  Speed: medium
+# New x position: 2
