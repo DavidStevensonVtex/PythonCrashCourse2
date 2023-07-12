c2e4cdca9bc65b312129e69e84dd8d7cb6aeaccd
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 18:01:25 -0400

    Error accessing non-existent dictionary key

---
+++ ch06/alien_no_points.py
@@ -0,0 +1,6 @@
+alien_0 = { 'color': 'green', 'speed': 'slow' }
+print(alien_0['points'])
+# Traceback (most recent call last):
+#   File "D:\drs\Python\PythonCrashCourse2\ch06\alien_no_points.py", line 2, in <module>
+#     print(alien_0['points'])
+# KeyError: 'points'
