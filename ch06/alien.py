f96d8095eb348c6f130391f5d0f660b2040f1de0
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 18:43:25 -0400

    Nesting: A List of Dictionaries

--- ch06/alien.py
+++ ch06/alien.py
@@ -1,7 +1,26 @@
-alien_0 = { 'color': 'green', 'points': 5 }
-print(alien_0)
+alien_0 = { 'color': 'green',  'points': 5 }
+alien_1 = { 'color': 'yellow', 'points': 10 }
+alien_2 = { 'color': 'red',    'points': 15 }
 
-del alien_0['points']
-print(alien_0)
+aliens = [ alien_0, alien_1, alien_2 ]
+
+for alien in aliens:
+    print(alien)
+
+print("\n")
+aliens = []
+aliens.append(alien_0)
+aliens.append(alien_1)
+aliens.append(alien_2)
+
+for alien in aliens:
+    print(alien)
+
 # {'color': 'green', 'points': 5}
-# {'color': 'green'}
+# {'color': 'yellow', 'points': 10}
+# {'color': 'red', 'points': 15}
+
+
+# {'color': 'green', 'points': 5}
+# {'color': 'yellow', 'points': 10}
+# {'color': 'red', 'points': 15}
