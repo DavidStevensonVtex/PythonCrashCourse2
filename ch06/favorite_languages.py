acd29b3ed1b684ff78b7a226707676e87172a6c9
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 17:56:36 -0400

    A Dictionary of Similar Objects

---
+++ ch06/favorite_languages.py
@@ -0,0 +1,14 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+print(favorite_languages)
+
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}")
+
+# {'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python'}
+# Sarah's favorite language is C
