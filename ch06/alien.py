01ab358189f62b0c78914c44004663c2008b4f25
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 17:42:53 -0400

    Modifying Values in a Dictionary

--- ch06/alien.py
+++ ch06/alien.py
@@ -1,5 +1,7 @@
-alien_0 = {}
-alien_0['color'] = 'green'
-alien_0['points'] = 5
-print(alien_0)
-# {'color': 'green', 'points': 5}
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
+# The alien is green.
+# The alien is now yellow.
