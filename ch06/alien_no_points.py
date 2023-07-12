3b33b53cf8c0ce90fd09d65a6870c2eda090810d
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 18:03:06 -0400

    Using get() to Access Values

--- ch06/alien_no_points.py
+++ ch06/alien_no_points.py
@@ -1,6 +1,5 @@
 alien_0 = { 'color': 'green', 'speed': 'slow' }
-print(alien_0['points'])
-# Traceback (most recent call last):
-#   File "D:\drs\Python\PythonCrashCourse2\ch06\alien_no_points.py", line 2, in <module>
-#     print(alien_0['points'])
-# KeyError: 'points'
+
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
+# No point value assigned.
