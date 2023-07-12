f8b2d621b3e3d4c3b741b27e7b15678b937b3169
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 17:39:36 -0400

    Starting with an Empty Dictionary

--- ch06/alien.py
+++ ch06/alien.py
@@ -1,6 +1,5 @@
-alien_0 = { 'color': 'green', 'points': 5 }
-
-alien_0['x_position'] = 0 
-alien_0['y_position'] = 25
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
 print(alien_0)
-# {'color': 'green', 'points': 5, 'x_position': 0, 'y_position': 25}
+# {'color': 'green', 'points': 5}
