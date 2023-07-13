ffd73be243e02cb65cc17ebccf6c7f108aade11f
David Stevenson <drstevenson1958@yahoo.com>
2023-07-13 16:58:55 -0400

    Using break to Exit a Loop

--- ch07/parrot.py
+++ ch07/parrot.py
@@ -1,10 +1,9 @@
 prompt = "\nTell me something, and I will repeat it back to you: "
 prompt += "\nEnter 'quit' to end the program. "
 
-active = True
-while active:
+while True:
     message = input(prompt)
     if message == 'quit':
-        active = False
+        break
     else:
         print(message)
