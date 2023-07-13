74300931e80585810f8c154034ab0e0c2ebad67d
David Stevenson <drstevenson1958@yahoo.com>
2023-07-13 17:45:04 -0400

    Avoiding infinite loops

--- ch07/counting.py
+++ ch07/counting.py
@@ -1,6 +1,4 @@
-current_number = 0
-while current_number < 10:
-    current_number += 1
-    if current_number % 2 == 0:
-        continue
-    print(current_number)
+x = 1
+while x <= 5:
+    print(x)
+    x += 1      # Omit this line, and this loop will run forever!
