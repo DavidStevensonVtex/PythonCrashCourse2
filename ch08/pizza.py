7035c64cb9bcf83923343f415d453e318999fba4
David Stevenson <drstevenson1958@yahoo.com>
2023-07-14 19:24:50 -0400

    Summarize the pizza we are about to make

--- ch08/pizza.py
+++ ch08/pizza.py
@@ -1,6 +1,8 @@
 def make_pizza(*toppings):
-    """print the list of toppings that have been requested."""
-    print(toppings)
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a piza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
 
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green_peppers', 'extra_cheese')
