1df8fd649f0487e01cb4dcb09d1ad8eb026b6c34
David Stevenson <drstevenson1958@yahoo.com>
2023-07-14 17:12:15 -0400

    Default Values, Keyword Arguments and Positional Parameters

--- ch08/pets.py
+++ ch08/pets.py
@@ -4,3 +4,8 @@ def describe_pet(pet_name, animal_type = 'dog'):
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 describe_pet(pet_name='willie')
+
+describe_pet('willie')
+
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet('harry', 'hamster')
