edb3020fdb65ae5acf68d3bc281805171872b3cc
David Stevenson <drstevenson1958@yahoo.com>
2023-07-18 20:08:14 -0400

    Validate updates to attributes

--- ch09/car.py
+++ ch09/car.py
@@ -16,10 +16,20 @@ class Car:
     def read_odometer(self):
         """Print a statement showing the car's mileage."""
         print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the dometer reading to the given value.
+        Reject the change if it attempts to foll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
     
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
 # my_new_car.read_odometer()
 
-my_new_car.odometer_reading = 23
+my_new_car.update_odometer(23)
 my_new_car.read_odometer()
