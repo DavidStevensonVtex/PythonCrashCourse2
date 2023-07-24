350f30c466824aa2a42f6371967b85ceb0f7899a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-24 15:45:47 -0400

    Adding another method to the Battery class.

--- ch09/electric_car.py
+++ ch09/electric_car.py
@@ -45,6 +45,15 @@ class Battery:
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        
+        print(f"This car can go about {range} miles on a full charge.")
+
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles."""
 
@@ -64,8 +73,8 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
-my_tesla.fill_gas_tank()
+my_tesla.battery.get_range()
 
 # 2019 Tesla Model S
 # This car has a 75-kWh battery.
-# This car doesn't need a gas tank!
+# This car can go about 260 miles on a full charge.
