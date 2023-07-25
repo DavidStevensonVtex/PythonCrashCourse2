af65ea992899957d2b624cf91613dae6e2269bef
David Stevenson <drstevenson1958@yahoo.com>
2023-07-25 16:46:09 -0400

    Working with Multiple Files - 2

--- ch10/word_count.py
+++ ch10/word_count.py
@@ -12,5 +12,11 @@ def count_words(filename):
         num_words = len(words)
         print(f"The file {filename} has about {num_words} words.")
 
-filename = 'alice.txt'
-count_words(filename)
+filenames = [ 'alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt' ]
+for filename in filenames:
+    count_words(filename)
+
+# The file alice.txt has about 29465 words.
+# Sorry, the file siddhartha.txt does not exist.
+# The file moby_dick.txt has about 215830 words.
+# The file little_women.txt has about 189079 words.
