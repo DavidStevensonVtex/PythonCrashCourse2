49f870f2f217ff76b93e70444ad895b9905c79c4
David Stevenson <drstevenson1958@yahoo.com>
2023-07-25 15:47:53 -0400

    Not Handling the Zero Division Error Exception

---
+++ ch10/division_calculator.py
@@ -0,0 +1,7 @@
+print(5/0)
+
+# Traceback (most recent call last):
+#   File "D:\drs\Python\PythonCrashCourse2\ch10\division_calculator.py", line 1, in <module>
+#     print(5/0)
+#           ~^~
+# ZeroDivisionError: division by zero
