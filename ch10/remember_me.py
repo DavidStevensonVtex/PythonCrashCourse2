a3ec225ade42920e1b8d2371c4c549a3fe2fd3f9
David Stevenson <drstevenson1958@yahoo.com>
2023-07-25 18:03:22 -0400

    Refactor #3

--- ch10/remember_me.py
+++ ch10/remember_me.py
@@ -10,6 +10,14 @@ def get_stored_username():
         return None
     else:
         return username
+    
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename ='username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username 
 
 def greet_user():
     """Greet the user by name."""
@@ -17,10 +25,7 @@ def greet_user():
     if username:
         print(f"Welcome back, {username}")
     else:
-        username = input("What is your name? ")
-        filename ='username.json'
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"We'll remember you when you come back, {username}!")
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
