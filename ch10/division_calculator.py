623d3297a9ea78eb25af2908703738b9ea895c97
David Stevenson <drstevenson1958@yahoo.com>
2023-07-25 15:57:10 -0400

    Using Exceptions to Prevent Crashes

--- ch10/division_calculator.py
+++ ch10/division_calculator.py
@@ -1,7 +1,20 @@
-print(5/0)
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
 
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    answer = int(first_number) / int(second_number)
+    print(answer)
+
+# First number: 123
+# Second number: 0
 # Traceback (most recent call last):
-#   File "D:\drs\Python\PythonCrashCourse2\ch10\division_calculator.py", line 1, in <module>
-#     print(5/0)
-#           ~^~
+#   File "D:\drs\Python\PythonCrashCourse2\ch10\division_calculator.py", line 11, in <module>
+#     answer = int(first_number) / int(second_number)
+#              ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~
 # ZeroDivisionError: division by zero
