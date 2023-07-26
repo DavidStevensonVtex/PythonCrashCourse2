32aae5e311578fed889b9358ad0736882e484110
David Stevenson <drstevenson1958@yahoo.com>
2023-07-26 15:00:55 -0400

    Implement Right Arrow Keydown and Keyup logic

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -21,8 +21,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            
-            # Redraw the screen during each pass through the loop.
+            self.ship_update()
             self._update_screen()
 
     def _check_events(self):
@@ -30,6 +29,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
