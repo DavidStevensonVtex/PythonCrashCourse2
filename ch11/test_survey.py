9ff7ea03307a006f946db7f6f9e3a9b671655086
David Stevenson <drstevenson1958@yahoo.com>
2023-07-26 08:20:35 -0400

    Using the setUp() unit test method

--- ch11/test_survey.py
+++ ch11/test_survey.py
@@ -4,23 +4,24 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     """Tests for the class AnonymousSurvey."""
 
+    def setUp(self) -> None:
+        """Create a survey and a set of responses for use in all test methods."""
+        self.question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(self.question)
+        self.responses = [ 'English', 'Spanish', 'Mandarin' ]
+
     def test_store_single_response(self):
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
-        self.assertEqual(question, my_survey.question)
+        self.my_survey.store_response('English')
+        self.assertIn('English', self.my_survey.responses)
+        self.assertEqual(self.question, self.my_survey.question)
 
     def test_store_three_responses(self):
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        responses = [ 'English', 'Spanish', 'Mandarin' ]
-        for response in responses:
-            my_survey.store_response(response)
+        for response in self.responses:
+            self.my_survey.store_response(response)
         
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
-        self.assertEqual(question, my_survey.question)       
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+        self.assertEqual(self.question, self.my_survey.question)       
 
 if __name__ == '__main__':
     unittest.main()
