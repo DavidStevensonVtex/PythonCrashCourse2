110f7532c446fd87301b54af80efc33661609866
David Stevenson <drstevenson1958@yahoo.com>
2023-07-28 14:28:30 -0400

    Saving Your Plots Automatically

--- ch15/scatter_squares.py
+++ ch15/scatter_squares.py
@@ -9,4 +9,4 @@ ax.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues, s = 10)
 # Set the range for each axis.
 ax.axis( [ 0, 1_100, 0, 1_100_000 ])
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
