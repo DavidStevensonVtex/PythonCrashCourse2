6a3e37b8947d25dfd5239b674b2fba1165b6fcfd
David Stevenson <drstevenson1958@yahoo.com>
2023-07-28 14:03:57 -0400

    Calculating Data Automatically

--- ch15/scatter_squares.py
+++ ch15/scatter_squares.py
@@ -2,8 +2,11 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-x_values = [ 1, 2, 3, 4, 5 ]
-y_values = [ 1, 4, 9, 16, 25 ]
+x_values = range(1,1001)
+y_values = [ x**2 for x in x_values ]
 ax.scatter(x_values, y_values)
+
+# Set the range for each axis.
+ax.axis( [ 0, 1_100, 0, 1_100_000 ])
 
 plt.show()
