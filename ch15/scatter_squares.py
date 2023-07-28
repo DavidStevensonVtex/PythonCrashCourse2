87145c68998e7ba20d278c8512a1d6eef8ee5c86
David Stevenson <drstevenson1958@yahoo.com>
2023-07-28 14:15:33 -0400

    Using a Colormap

--- ch15/scatter_squares.py
+++ ch15/scatter_squares.py
@@ -4,7 +4,7 @@ plt.style.use('seaborn')
 fig, ax = plt.subplots()
 x_values = range(1,1001)
 y_values = [ x**2 for x in x_values ]
-ax.scatter(x_values, y_values, c = 'red', s = 10)
+ax.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues, s = 10)
 
 # Set the range for each axis.
 ax.axis( [ 0, 1_100, 0, 1_100_000 ])
