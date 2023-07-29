0638da063094c6a72bce284ef46a51e9fe993c4a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-29 13:14:24 -0400

    Extracting Magnitudes

--- ch16/mapping_global_data_sets/eq_explore_data.py
+++ ch16/mapping_global_data_sets/eq_explore_data.py
@@ -6,5 +6,12 @@ with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
-# 158
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+# Print first 10 magnitudes
+print(mags[:10])
+# [0.96, 1.2, 4.3, 3.6, 2.1, 4, 1.06, 2.3, 4.9, 1.8]
