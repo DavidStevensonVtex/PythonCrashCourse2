9666baf8e3ac6e7cf7ceade2c071a9c69b6f0905
David Stevenson <drstevenson1958@yahoo.com>
2023-08-07 16:36:00 -0400

    Protesting a User's Topics

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from learning_logs.models import Topic, Entry
 from learning_logs.forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 
 def index(request):
@@ -19,6 +20,10 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    #Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
